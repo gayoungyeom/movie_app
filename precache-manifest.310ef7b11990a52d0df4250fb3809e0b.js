0291dc7f5d3d08f1fb76493ce04c529c0cc6ef4c
Gayoung Yeom <gayoungyeom@gmail.com>
2020-01-29 20:27:38 +0900

    Updates

---
+++ precache-manifest.310ef7b11990a52d0df4250fb3809e0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94cccda2aab7593129145d66a0507fba",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "69d48f6e1599a98b82ca",
+    "url": "/movie_app/static/css/main.2fca4ab2.chunk.css"
+  },
+  {
+    "revision": "fd8eefd160e1aa99589d",
+    "url": "/movie_app/static/js/2.d2db0a13.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/movie_app/static/js/2.d2db0a13.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69d48f6e1599a98b82ca",
+    "url": "/movie_app/static/js/main.da2da3df.chunk.js"
+  },
+  {
+    "revision": "89493c886a09d05e6b0d",
+    "url": "/movie_app/static/js/runtime-main.a919813c.js"
+  }
+]);
